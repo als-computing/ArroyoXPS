e576ba59a27885287b6b45ff173672984f199ac5
Runbo Jiang <rjiang2@lbl.gov>
2024-05-20 15:37:31 -0700

    Add test peak fitting code and example data

--- src/_tests/test_calculate.py
+++ src/_tests/test_calculate.py
@@ -1,33 +1,24 @@
 import numpy as np
 import collections
-import matplotlib.pyplot as plt
-from peak_fitting import get_peaks
+import sys
+sys.path.append('../')
+from tr_ap_xps.peak_fitting import get_peaks
+# from tr_ap_xps.calculate import calculate
 
-def test():
-    """
-    This function will plot the peak locations for the entire data
-    """
-    file_path = "/Users/runbojiang/Desktop/AP-XPS-my/avg_array_3.npy"
+
+def test_peak_detection():
+    file_path = "test_array_300_1131.npy"
     array = np.load(file_path)
-    print(array.shape)
-    x = np.arange(array.shape[1])
-    peak_locations = collections.defaultdict(list)
-    # for i in range(array.shape[0]):
-    for i in range(300):
-        y = array[i, :]
-        return_list, unfit_list, fit_list, residual, base_list = get_peaks(x, y, 2, 'g')
-        for peak in return_list:
-            peak_locations[i].append(peak['index'])
+    assert array.shape[0] == 300
 
-    # plot the location of first and second peak
-    for key in peak_locations:
-        peak_locations[key].sort()
-    keys = list(peak_locations.keys())
-    first_peak_locations = [peak_locations[key][0] for key in keys]
-    second_peak_locations = [peak_locations[key][1] for key in keys]
-    plt.figure(figsize=(10, 5))
-    plt.plot(keys, first_peak_locations, label='First Peak Mean', marker='o')
-    plt.plot(keys, second_peak_locations , label='Second Peak Mean', marker='o')
-    plt.grid(True)
-    # Show plot
-    plt.show()
+    x = np.arange(array.shape[1])
+    y = array[-1, :]
+    return_list, unfit_list, fit_list, residual, base_list = get_peaks(x, y, 2, 'g')
+    peak_locations = [peak['index'] for peak in return_list]
+    assert len(peak_locations) == 2
+    assert peak_locations[0] in range(300, 400) and peak_locations[1] in range(600, 700)
+
+
+
+if __name__ == "__main__":
+    test_peak_detection()

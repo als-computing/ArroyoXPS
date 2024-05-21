1c26cf42b78023f4a36edf7c024989123897e2c6
Runbo Jiang <rjiang2@lbl.gov>
2024-05-21 09:54:19 -0700

    Remove plot related code

--- src/tr_ap_xps/peak_fitting.py
+++ src/tr_ap_xps/peak_fitting.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 from scipy import signal
 from astropy.modeling import models, fitting
-import matplotlib.pyplot as plt
-import collections
-import time
+
 
 # find the bins
 def bayesian_block_finder(x: np.ndarray=np.ones(5,), y: np.ndarray=np.ones(5,),):
@@ -320,38 +318,3 @@ def peak_fit(array):
         peak_location.append(peak['index'])
     peak_location.sort()
     return np.array(peak_location)
-
-def plot_peak_locations_for_raw_data():
-    """
-    This function will plot the peak locations for the entire data with path specified
-    """
-    file_path = "/Users/runbojiang/Desktop/AP-XPS-my/avg_array_3.npy"
-    array = np.load(file_path)
-    print(array.shape)
-    sub_array = array[:300, :]
-    np.save('test_array_300_1131.npy', sub_array)
-    x = np.arange(array.shape[1])
-    peak_locations = collections.defaultdict(list)
-    start_time = time.time()
-    # for i in range(array.shape[0]):
-    for i in range(300):
-        y = array[i, :]
-        return_list, unfit_list, fit_list, residual, base_list = get_peaks(x, y, 2, 'g')
-        for peak in return_list:
-            peak_locations[i].append(peak['index'])
-
-    print(time.time() - start_time, "s") # 28 s for 300 frames
-    # plot the location of first and second peak
-    for key in peak_locations:
-        peak_locations[key].sort()
-    keys = list(peak_locations.keys())
-    first_peak_locations = [peak_locations[key][0] for key in keys]
-    second_peak_locations = [peak_locations[key][1] for key in keys]
-    plt.figure(figsize=(10, 5))
-    plt.plot(keys, first_peak_locations, label='First Peak Mean', marker='o')
-    plt.plot(keys, second_peak_locations , label='Second Peak Mean', marker='o')
-    plt.grid(True)
-    plt.show()
-
-if __name__ == "__main__":
-    test()

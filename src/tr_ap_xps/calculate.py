d91663512f738fefd7a030ece2fe9fb9365e2f2a
Runbo Jiang <rjiang2@lbl.gov>
2024-05-21 12:00:35 -0700

    Move dataclass to calculate.py

--- src/tr_ap_xps/calculate.py
+++ src/tr_ap_xps/calculate.py
@@ -1,8 +1,20 @@
+from dataclasses import dataclass
+
 import numpy as np
 
-from .calculated_item import CalculatedItem
 from .fft_calculating import calculate_fft_items
 from .peak_fitting import peak_fit
+
+
+@dataclass
+class CalculatedItem:
+    """Class for keeping track of all calculated items."""
+
+    integrated_frame: np.array
+    detected_peaks: np.array
+    vfft: np.array
+    ifft: np.array
+    sum: np.array
 
 
 def calculate(array: np.array, repeat_factor=20):

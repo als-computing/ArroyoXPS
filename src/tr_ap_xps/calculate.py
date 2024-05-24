1d155039abe324201ff0c3659fedf241ce9c9947
Runbo Jiang <rjiang2@lbl.gov>
2024-05-24 10:39:23 -0700

    Seperate integrate and fft calculation

--- src/tr_ap_xps/calculate.py
+++ src/tr_ap_xps/calculate.py
@@ -3,8 +3,10 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
-from .fft import calculate_fft_items
-from .peak_fitting import peak_fit
+# from .fft import calculate_fft_items
+# from .peak_fitting import peak_fit
+from fft import calculate_fft_items
+from peak_fitting import peak_fit
 
 
 @dataclass
@@ -17,15 +19,23 @@ class CalculatedItem:
     ifft: np.array
     sum: np.array
 
+def integrate_frame(array: np.array):
+    """
+    This function integrate the input 2d raw frame into 1d.
+    """
 
-def calculate(array: np.array, repeat_factor=20):
-    """
-    Array is of shape (frame index, x, y)
-    """
-    curr_frame = array[-1, :, :]
-    integrated_frame = np.mean(curr_frame, axis=0)
+    integrated_frame = np.mean(array, axis=0)
     detected_peaks = peak_fit(integrated_frame)
-    vfft, sum, ifft = calculate_fft_items(array, repeat_factor)
+
+    return integrated_frame, detected_peaks
+
+
+def calculate(raw_frame: np.array, stacked_integrated_frames: np.array, repeat_factor=20):
+    assert raw_frame.ndim == 2, "The current raw_frame is not 2D."
+    assert stacked_integrated_frames.ndim == 2, "The stacked_integrated_frames array is not 2D."
+
+    integrated_frame, detected_peaks = integrate_frame(raw_frame)
+    vfft, sum, ifft = calculate_fft_items(stacked_integrated_frames, repeat_factor=repeat_factor)
 
     result = CalculatedItem(integrated_frame, detected_peaks, vfft, sum, ifft)
     return result

33fb1985e296f6dca24836e2832eb716448b7cf0
Dylan McReynolds <dmcreynolds@lbl.gov>
2024-05-24 18:20:47 -0700

    append new frame to lines_raw

--- src/_tests/test_writer.py
+++ src/_tests/test_writer.py
@@ -25,25 +25,8 @@ def test_XPSDataSet_init(client, integrated_frame):
         10,
     ), "Expected a single row with 10 columns, matching a veritcally integrated frame"
 
-
-# def test_update_zarr(tmpdir):
-#     # Test that the writer can append a 2D chunk to a 3D Zarr array
-#     # Create a Zarr array
-#     zarr_location = str(tmpdir.join("test.zarr"))
-#     writer = ResultStore(zarr_location, "run42", (5, 10), np.int32)
-#     assert zarr.open(zarr_location) is not None
-#     assert writer.run_group is not None
-#     assert writer.results_group is not None
-#     assert writer.v_integrated_frame_dataset is not None
-#     assert writer.vfft_dataset is not None
-#     assert writer.ifft_dataset is not None
-#     assert writer.sum_data_dataset is not None
-
-#     # # Append a 1D chunk
-#     # chunk_1d = np.random.rand(10).astype(np.int32)
-#     # writer.append_1d_chunk(writer.vfft_dataset, chunk_1d)
-#     # assert writer.vfft_dataset.shape == (1, 10)
-#     # assert np.allclose(writer.vfft_dataset[0], chunk_1d)
-
-#     new_frame = np.random.randint(0, 100, size=(5, 10), dtype='int32')
-#     writer.new_integrated_frame(new_frame)
+    xps_dataset.new_integrated_frame(integrated_frame)
+    assert xps_dataset.lines_raw_node.read().shape == (
+        2,
+        10,
+    ), "Expected another row of verically integrated frame"
--- src/tr_ap_xps/writer.py
+++ src/tr_ap_xps/writer.py
@@ -33,6 +33,7 @@ class XPSDataSet:
             )
             frame.write(data_frame)
             return frame
+        tiled_node[name].append_partition(data_frame, 0)
         return tiled_node[name]
 
     def new_integrated_frame(self, curr_frame: np.array):
@@ -44,108 +45,3 @@ class XPSDataSet:
         self.lines_raw_node = self._get_or_create_table(
             self.run_node, new_integrated_frame, "lines_raw"
         )
-
-
-# class ZarrResultStore:
-#     # so experiemental that it barely works
-#     def __init__(self, zarr_location: str, run_name: str, frame_shape: tuple, dtype=np.dtype) -> None:
-#         self.frame_shape = frame_shape
-
-#         # Create or open a Zarr array with the ability to resize
-#         self.store = zarr.DirectoryStore(zarr_location.join(run_name))
-#         self.run_group = zarr.group(self.store)
-
-#         # Create a group to store the results
-#         if "results" not in self.run_group:
-#             self.results_group = self.run_group.create_group("results")
-#         self.results_group = self.run_group["results"]
-
-#         shape_3d = (0, *frame_shape)
-
-#         # # Create a dataset to store a 2D array, height will be number of frames, width same as frame width
-#         self.v_integrated_frame_dataset = self._get_or_create_dataset(
-#             self.results_group, "v_integrated_frame", (0, self.frame_shape[1]), np.float64,
-#         )
-#         self.vfft_dataset = self._get_or_create_dataset(
-#             self.results_group, "vfft", (0, 0, frame_shape[1]), np.float64
-#         )
-#         self.ifft_dataset = self._get_or_create_dataset(
-#             self.results_group, "ifft", shape_3d, np.float64
-#         )
-#         self.sum_data_dataset = self._get_or_create_dataset(
-#             self.results_group, "sum_data", shape_3d, np.float64
-#         )
-
-#     def _get_or_create_dataset(
-#         self, group, dataset_name: str, shape: tuple, dtype: np.dtype
-#     ):
-#         # Create the shape and maxshape dynamically based on dims
-#         maxshape = (None,) * len(shape)
-#         # Check if the array already exists
-#         if dataset_name not in group:
-#             return group.create_dataset(
-#                 dataset_name,
-#                 shape=shape,
-#                 dtype=dtype,
-#                 chunks=True,
-#                 maxshape=maxshape,
-#             )
-#         return group[dataset_name]
-
-#     def new_integrated_frame(self, curr_frame: np.array):
-#         # Every time a new frame comes in, we will:
-#         #  1. Integrate the frame along the vertical axis
-#         #  2. Append the integrated frame to the raw dataset
-#         #  3. Append the filtered (vfft, filter, ifft) frame to the result dataset
-#         #  . Fit the filtered image and append the fitted peak to the trending dataset
-#         assert curr_frame.shape == self.frame_shape,
-# f"Frame width does not match. {self.frame_shape=}, {curr_frame.shape=}"
-#         new_integrated_frame = np.mean(curr_frame, axis=0)
-#         self.append_1d_chunk(self.v_integrated_frame_dataset, new_integrated_frame)
-
-#         vfft = get_vfft(self.v_integrated_frame_dataset)
-#         self.append_2d_chunk(self.vfft_dataset, vfft)
-
-#     # Function to append a 1D chunk to the 3D array along the z-axis
-#     def append_1d_chunk(self, dataset, chunk_1d: np.array, axis=0):
-#         assert (
-#             len(chunk_1d) == self.frame_shape[1]
-#         ), f"Chunk length does not match the frame width. {self.frame_shape=}, {len(chunk_1d)=}"
-#         # Current shape of the Zarr array
-#         shape = list(dataset.shape)
-
-#         # Determine the new shape after appending the 2D chunk
-#         new_shape = shape[:]
-#         new_shape[axis] += 1
-
-#         # Resize the Zarr array to accommodate the new chunk
-#         dataset.resize(tuple(new_shape))
-
-#         # Determine the slice to insert the new chunk
-#         slice_obj = [slice(None)] * len(shape)
-#         slice_obj[axis] = shape[axis]
-
-#         # Insert the new chunk
-#         dataset[tuple(slice_obj)] = chunk_1d
-
-#     # Function to append a 2d chunk to the 3D array along the z-axis
-#     def append_2d_chunk(self, dataset, chunk_2d: np.array, axis=0):
-#         assert (
-#             chunk_2d.shape == self.frame_shape
-#         ), f"Chunk length does not match the frame width. {self.frame_shape=}, {chunk_2d.shape=}"
-#         # Current shape of the Zarr array
-#         shape = list(dataset.shape)
-
-#         # Determine the new shape after appending the 2D chunk
-#         new_shape = shape[:]
-#         new_shape[axis] += 1
-
-#         # Resize the Zarr array to accommodate the new chunk
-#         dataset.resize(tuple(new_shape))
-
-#         # Determine the slice to insert the new chunk
-#         slice_obj = [slice(None)] * len(shape)
-#         slice_obj[axis] = shape[axis]
-
-#         # Insert the new chunk
-#         dataset[tuple(slice_obj)] = chunk_2d

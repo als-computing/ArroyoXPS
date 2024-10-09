a412247465b6dc03491a236880f731306c05d245
Dylan McReynolds <dmcreynolds@lbl.gov>
2024-10-09 09:29:48 -0700

    rearrange labview messages

--- src/tr_ap_xps/schemas.py
+++ src/tr_ap_xps/schemas.py
@@ -1,49 +1,24 @@
 from typing import Literal
 
-import numpy
-import pandas
-from arroyo.schemas import Event, Message, Start, Stop
-from pydantic import BaseModel, Field, field_validator
+from arroyo.schemas import DataFrameModel, Event, Message, NumpyArrayModel, Start, Stop
+from pydantic import BaseModel, Field
 
+"""
+    This module defines schemas for XPS (X-ray Photoelectron Spectroscopy) messages and events using
+    Pydantic models. It includes classes for different types of messages and events such as
+    start, stop, raw events, and results. These classes serve as data transfer classes within the
+    tr_ap_xps pipeline.
 
-class DataFrameModel(BaseModel):
-    """
-    A Pydantic model for validating pd.DataFrame objects.
-    Does not parse array, merely validates that is a pd.DataFrame
-    """
+    Pydantic is used for several reasons.
+    - It provides validated of messages
+    - Using pydantic's alias mechanism, it provides a mapping between the json field names produced by LabVIEW and
+        python field name.
+    - Pydantic provides easy translation between json and python structures
 
-    df: pandas.DataFrame
+    Three of these models define the incoming message from LabView, one defines the outgoing message
+    from our Operators.
 
-    @field_validator("df", mode="before")
-    def validate_is_numpy_array(cls, v):
-        if not isinstance(v, pandas.DataFrame):
-            raise TypeError(f"Expected pd.DataFrame, got {type(v)} instead.")
-        return v  # Do not modify or parse the array
-
-    class Config:
-        arbitrary_types_allowed = True  # Allow numpy.ndarray type
-
-
-class NumpyArrayModel(BaseModel):
-    """
-    A Pydantic model for validating numpy.ndarray objects.
-    Does not parse array, merely validates that is a np.ndarray
-    """
-
-    array: numpy.ndarray
-
-    @field_validator("array", mode="before")
-    def validate_is_numpy_array(cls, v):
-        if not isinstance(v, numpy.ndarray):
-            raise TypeError(f"Expected numpy.ndarray, got {type(v)} instead.")
-        return v  # Do not modify or parse the array
-
-    class Config:
-        arbitrary_types_allowed = True  # Allow numpy.ndarray type
-
-
-class XPSMessage(Message):
-    pass
+"""
 
 
 class Rectangle(BaseModel):
@@ -54,10 +29,55 @@ class Rectangle(BaseModel):
     rotation: int = Field(..., alias="Rotation")
 
 
+class XPSMessage(Message):
+    pass
+
+
 class XPSStart(Start, XPSMessage):
     msg_type: str = Literal["start"]
     binding_energy: float = Field(..., alias="Binding Energy")
     msg_type: str = Field("start", alias="msg_type")
+    """
+    LabVIEW Message:
+
+    Incoming message from LabView at the start of a scan.
+    Expects incoming message to be JSON.
+    An example with nonsense values:
+
+    {
+        "XPSStart": {
+            "Binding Energy (eV)": 42.0001,
+            "frame_per_cycle": 10,
+            "scan_name": "Don't panic!",
+            "F_trigger": 42,
+            "F_Un-Trigger": 42,
+            "F_Dead": 42,
+            "F_Reset": 42,
+            "CCD_nx": 42,
+            "CCD_ny": 42,
+            "Pass Energy": 42.0001,
+            "Center Energy": 42.001,
+            "Offset Energy": 42.001,
+            "Lens Mode: "Magrathea",
+            "Rectangle": {
+                "Left": 42,
+                "Top": 42,
+                "Right": 42,
+                "Bottom": 42
+            },
+            "Notes": "So long and thanks for all the fish",
+            "dt": 42.00001,
+            "Photon Energy": 42.0001,
+            "Binding Energy": 42.0001,
+            "File Ver": "Earth V2"
+            "Stream": "Heart of Gold"
+        }
+    }
+
+    """
+
+    binding_energy: float = Field(..., alias="Binding Energy (eV)")
+    frames_per_cycle: int = Field(..., alias="frame_per_cycle")
     scan_name: str = Field(..., alias="scan_name")
     f_trigger: int = Field(..., alias="F_Trigger")
     f_untrigger: int = Field(..., alias="F_Un-Trigger")
@@ -93,7 +113,37 @@ class XPSStop(Stop, XPSMessage):
     pass
 
 
+class XPSImageInfo(BaseModel):
+    """
+    LabVIEW Message:
+
+        Incoming mesesage with every frame.
+        For each frame, LabVIEW sends a json document with frame information.
+        Expects incoming message to be JSON.
+        An example with nonsense values:
+
+        {
+            "XPSImageInfo": {
+                "Frame Number": 42,
+                "Width": 1042,
+                "Height": 1042,
+                "Data Type": "U8"
+            }
+        }
+    """
+
+    frame_number: int = Field(..., alias="Frame Number")
+    width: int = Field(..., alias="Width")
+    height: int = Field(..., alias="Height")
+    data_type: str = Field(..., alias="data_type")
+
+
 class XPSResult(Event, XPSMessage):
+    """
+    This model is output from Operators and used by Publishers after
+    calculations are made.
+    """
+
     frame_number: int
     integrated_frames: NumpyArrayModel
     detected_peaks: DataFrameModel

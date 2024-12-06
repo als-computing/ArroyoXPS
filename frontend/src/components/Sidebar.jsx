57f81ab6902f38fcd53b584bafdc912420ed609c
seijdeleon <seijdeleon@lbl.gov>
2024-12-06 13:08:56 -0800

    new UI layout with header sidebar and main

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+    return (
+        <aside className="bg-slate-200 min-w-64 flex-shrink-0 shadow-md">
+            <h2>Websocket</h2>
+        </aside>
+    )
+}

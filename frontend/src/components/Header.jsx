585892ec416e8782473ff621d76cc811f8c8b466
seijdeleon <seijdeleon@lbl.gov>
2024-12-09 14:05:40 -0800

    revise favicon, doc title, and heading

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return(
         <header className="flex items-center h-8 py-8 justify-center space-x-4">
             <img src={als_logo} alt="als logo" className="h-8 w-auto"/>
-            <h1 className="text-4xl text-sky-900">AP-XPS Live Data Viewer</h1>
+            <h1 className="text-4xl text-sky-900">TR-AP-XPS Live Data Viewer</h1>
         </header>
     )
 }
